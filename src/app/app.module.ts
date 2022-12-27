72767713e46af92d25570c181e3ad1bbafaee0ed
Natis <natijago@gmail.com>
2022-12-27 10:20:01 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingpageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
