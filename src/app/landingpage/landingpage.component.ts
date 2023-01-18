77277404c4d0c634aab08f0613a67b7f784b3d7b
Natis <natijago@gmail.com>
2023-01-18 10:28:05 -0500

    Finalizacion de fase colaborativa, cierre de proyecto en acompanamiento

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class LandingpageComponent {
 
+//FUNCIÓN QUE APLICA ESTILO A OPCIÓN SELECCIONADA Y QUITA EL ESTILO A LA SELECCIÓN ANTERIOR
+  seleccionar(link:any) {
+    let opciones:any = document.querySelectorAll('#links a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+  // Desaparecemos el menu una vez seleccionada una opción en el modo responsive
+  var x:any = document.getElementById("nav");
+  x.className = "";
+}
+
+// FUNCIÓN QUE MUESTRA EL MENU RESPONSIVE
+  responsiveMenu(){
+    var x:any = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+      x.className = "";
+    }
+  }
 }
