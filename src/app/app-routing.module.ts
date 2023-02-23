87831dd41c4948c20707d56d26ac65bf5aeb1420
Natis <natijago@gmail.com>
2023-02-23 13:33:25 -0500

    Actualizacion HV

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
-const routes: Routes = [];
+const routes: Routes = []
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
